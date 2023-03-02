cc2fd04e4e1cc1ddc73d2b98c1027f68acad2817
Trent Houle <trenthoule@Trents-MacBook-Air-3.local>
2023-03-02 03:46:31 -0600

    what is going on

--- Lists2.py
+++
@@ -1,8 +0,0 @@
-def min_in_neighborhood(values, location):
-    assert len(values) > 0 
-    assert 0 <= location < len(values)
-    "return the lowest value of neighbors in a string"
-    if location == 0:
-        if values[location] < values[location + 1]:
-            return values[location]
-        return values[location + 1]
--- Useful code.py
+++
@@ -1,9 +0,0 @@
-""" Useful code snippets from class and elsewhere
-"""
-
-from os import path, listdir
-import sys
-
-if __name__ == "__main__":
-    print(sys.argv)
-
